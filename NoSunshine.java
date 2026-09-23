f2cb2b5905a380e0915497949d7751e1bef84454
TheManOfBred <112578850+TheManOfBred@users.noreply.github.com>
2026-09-23 11:42:12 -0700

    Create NoSunshine.java

---
+++ NoSunshine.java
@@ -0,0 +1,27 @@
+public class NoSunshine{
+public static void main(String[] args) {
+String time="Anytime she goes away";
+String house="And this house just ain't no home";
+String kn="I know, ";
+v1();
+print("It's not warm when she's away");
+v1();
+print("And she's always gone too long\n"+time+"\n\nWonder this time where she's gone\nWonder if she's gone to stay");
+v1();
+print(house+"\n"+time+"\n");
+print("And "+kn+kn+kn+"I know");
+know();
+know();
+know();
+know();
+print(kn+"I know\n\nHey, I ought to leave young thing alone\nBut ain't no sunshine when she's gone, whoa-whoa");
+v1();
+print("Only darkness every day\n");
+v1();
+print(house+"\n\n"+time+"\n"+time+"\n"+time+"\n"+time);}
+public static void v1() {
+System.out.println("Ain't no sunshine when she's gone");}
+public static void print(String message){
+System.out.println(message);}
+public static void know() {
+System.out.println("I know, I know, I know, I know, I know");}}
